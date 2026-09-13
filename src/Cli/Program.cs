f7f1a51b6c4eff9c7d13a450a923f41d6d7c6a1a
Khinotska Sofiia bc2025-7 <Sofiia.Khinotska@lnu.edu.ua>
2026-09-13 18:46:46 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студентка: Хіноцька Софія, група ФеІ-37с");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription)   : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment)     : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу  : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR)    : {Environment.Version}");
+Console.WriteLine($"Runtime              : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку   : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог     : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область:  Бібліотека. Сутності: Book (видання), BookCopy (примірник), Reader (читач), Loan (видача). Призначення: облік видач примірників читачам і повернень.");
